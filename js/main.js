86a973a817909b71b3b9ebebb4368da99d3c5bf6
tairhiq <tairhiqharris@yahoo.com>
2024-12-04 11:05:45 -0600

    Text writer effect, images, grid
    Text writer effect, images, grid

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 // JS scripts placed here
+var typed = new Typed('.bold-intro', {
+    strings: ["Tai'rhiq", "a Artist", "a Designer", "a Problem Solver"],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop:true,
+});
+
+var typed = new Typed('.traditional', {
+    strings: ["Traditional Art", "Color Pencil", "Color Theory"],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop:true,
+});
